dc8eb94f93172292628e2f717781eecc389f81f7
Ludovic Raess <ludovic.rass@gmail.com>
2021-12-03 16:49:29 +0100

    Add reference tests.

---
+++ test/test_3D_3.jl
@@ -0,0 +1,16 @@
+
+include("./shared.jl")
+
+## 3D tests
+include("../scripts/stokes_3D/Stokes3D_ve_perf.jl")
+@reset_parallel_stencil()
+indsx = Int.(ceil.(LinRange(1, length(xc), 12)))
+indsy = Int.(ceil.(LinRange(1, length(yc), 12)))
+indsz = Int.(ceil.(LinRange(1, length(zc), 12)))
+d3d   = Dict(:X=> xc[indsx], :P=>P[indsx,indsy,indsz])
+
+@testset "Reference-tests Stokes 3D VE perf (noMPI)" begin
+    @test_reference "reftest-files/test_Stokes3D_ve_perf.bson" d3d by=comp
+end
+
+@reset_parallel_stencil()
