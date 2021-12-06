d2b089c5f00eefae98793a873810bf1aab58745d
Ivan Utkin <utkin@imec.msu.ru>
2021-12-06 15:01:12 +0100

    Add new systemaitics scripts

--- scripts/Stokes2D_param.jl
+++ scripts/Stokes2D_param.jl
@@ -1,13 +1,17 @@
-const USE_GPU = haskey(ENV, "USE_GPU") ? parse(Bool, ENV["USE_GPU"]) : false
-const do_viz  = haskey(ENV, "DO_VIZ")  ? parse(Bool, ENV["DO_VIZ"])  : true
-const do_save = haskey(ENV, "DO_SAVE") ? parse(Bool, ENV["DO_SAVE"]) : false
-const do_save_vis = haskey(ENV, "DO_SAVE_VIZ") ? parse(Bool, ENV["DO_SAVE_VIZ"]) : false
-const nx      = haskey(ENV, "RESOL") ? parse(Int, ENV["RESOL"]) : 512 - 1
-const ny      = haskey(ENV, "RESOL") ? parse(Int, ENV["RESOL"]) : 512 - 1
-const vrpow   = haskey(ENV, "VISCR") ? parse(Int, ENV["VISCR"]) : 9
-const nsub    = haskey(ENV, "NSUB") ? parse(Int, ENV["NSUB"]) : 3
-const Re_mlt  = haskey(ENV, "REMULT") ? parse(Float64, ENV["REMULT"]) : 1.0
-const outdir  = haskey(ENV, "OUTDIR") ? ENV["OUTDIR"] : "../output"
+const USE_GPU     = haskey(ENV, "USE_GPU"    ) ? parse(Bool   , ENV["USE_GPU"    ]) : true
+const do_viz      = haskey(ENV, "DO_VIZ"     ) ? parse(Bool   , ENV["DO_VIZ"     ]) : false
+const do_save     = haskey(ENV, "DO_SAVE"    ) ? parse(Bool   , ENV["DO_SAVE"    ]) : false
+const do_save_vis = haskey(ENV, "DO_SAVE_VIZ") ? parse(Bool   , ENV["DO_SAVE_VIZ"]) : false
+const gpu_id      = haskey(ENV, "GPU_ID"     ) ? parse(Int    , ENV["GPU_ID"     ]) : 0
+const nx          = haskey(ENV, "RESOL"      ) ? parse(Int    , ENV["RESOL"      ]) : 2048 - 1
+const ny          = haskey(ENV, "RESOL"      ) ? parse(Int    , ENV["RESOL"      ]) : 2048 - 1
+const vrpow       = haskey(ENV, "VISCR"      ) ? parse(Int    , ENV["VISCR"      ]) : 9
+const nsub        = haskey(ENV, "NSUB"       ) ? parse(Int    , ENV["NSUB"       ]) : 1
+const Re_mlt      = haskey(ENV, "REMULT"     ) ? parse(Float64, ENV["REMULT"     ]) : 1.0
+const ρg0         = haskey(ENV, "RHOG0"      ) ? parse(Float64, ENV["RHOG0"      ]) : 0.0
+const ρgi         = haskey(ENV, "RHOGI"      ) ? parse(Float64, ENV["RHOGI"      ]) : 0.0
+const εbg         = haskey(ENV, "EBG"        ) ? parse(Float64, ENV["EBG"        ]) : 1.0
+const simdir      = haskey(ENV, "SIMDIR"     ) ?                ENV["SIMDIR"     ]  : "../output"
 ###
 using ParallelStencil
 using ParallelStencil.FiniteDifferences2D
@@ -40,18 +44,20 @@ end
     return
 end
 
+macro μ_veτ()    esc(:( 1.0/(1.0/@all(Gdτ) + 1.0/@all(Mus)) )) end
+macro μ_veτ_av() esc(:( 1.0/(1.0/@av(Gdτ)  + 1.0/@av(Mus))  )) end
 @parallel function compute_τ!(τxx::Data.Array, τyy::Data.Array, τxy::Data.Array, Vx::Data.Array, Vy::Data.Array, Mus::Data.Array, Gdτ::Data.Array, dx::Data.Number, dy::Data.Number)
-    @all(τxx) = (@all(τxx) + 2.0*@all(Gdτ)*@d_xa(Vx)/dx)/(@all(Gdτ)/@all(Mus) + 1.0)
-    @all(τyy) = (@all(τyy) + 2.0*@all(Gdτ)*@d_ya(Vy)/dy)/(@all(Gdτ)/@all(Mus) + 1.0)
-    @all(τxy) = (@all(τxy) + 2.0*@av(Gdτ)*(0.5*(@d_yi(Vx)/dy + @d_xi(Vy)/dx)))/(@av(Gdτ)/@av(Mus) + 1.0)
+    @all(τxx) = 2.0*@μ_veτ()*(@d_xa(Vx)/dx + @all(τxx)/@all(Gdτ)/2.0)
+    @all(τyy) = 2.0*@μ_veτ()*(@d_ya(Vy)/dy + @all(τyy)/@all(Gdτ)/2.0)
+    @all(τxy) = 2.0*@μ_veτ_av()*(0.5*(@d_yi(Vx)/dy + @d_xi(Vy)/dx) + @all(τxy)/@av(Gdτ)/2.0)
     return
 end
 
-@parallel function compute_dV!(Rx::Data.Array, Ry::Data.Array, dVx::Data.Array, dVy::Data.Array, Pt::Data.Array, τxx::Data.Array, τyy::Data.Array, τxy::Data.Array, dτ_Rho::Data.Array, dx::Data.Number, dy::Data.Number)
-    @all(Rx)   = @d_xi(τxx)/dx + @d_ya(τxy)/dy - @d_xi(Pt)/dx
-    @all(Ry)   = @d_yi(τyy)/dy + @d_xa(τxy)/dx - @d_yi(Pt)/dy
-    @all(dVx)  = @av_xi(dτ_Rho)*@all(Rx)
-    @all(dVy)  = @av_yi(dτ_Rho)*@all(Ry)
+@parallel function compute_dV!(Rx::Data.Array, Ry::Data.Array, dVx::Data.Array, dVy::Data.Array, Pt::Data.Array, τxx::Data.Array, τyy::Data.Array, τxy::Data.Array, dτ_Rho::Data.Array, ρg::Data.Array, dx::Data.Number, dy::Data.Number)
+    @all(Rx)  = @d_xi(τxx)/dx + @d_ya(τxy)/dy - @d_xi(Pt)/dx
+    @all(Ry)  = @d_yi(τyy)/dy + @d_xa(τxy)/dx - @d_yi(Pt)/dy - @av_yi(ρg)
+    @all(dVx) = @av_xi(dτ_Rho)*@all(Rx)
+    @all(dVy) = @av_yi(dτ_Rho)*@all(Ry)
     return
 end
 
@@ -73,7 +79,7 @@ end
     return
 end
 
-@parallel_indices (ix,iy) function create_inclusions!(Mus::Data.Array, Xi::Data.Array, Yi::Data.Array, r2::Data.Number, mus0::Data.Number, musi::Data.Number, dx::Data.Number, dy::Data.Number, lx::Data.Number, ly::Data.Number)
+@parallel_indices (ix,iy) function create_inclusions!(Mus::Data.Array, ρg::Data.Array, Xi::Data.Array, Yi::Data.Array, r2::Data.Number, mus0::Data.Number, musi::Data.Number, ρg0::Data.Number, ρgi::Data.Number, dx::Data.Number, dy::Data.Number, lx::Data.Number, ly::Data.Number)
     if ix <= size(Mus,1) && iy <= size(Mus,2)
         x = (ix-1)*dx + 0.5*dx - 0.5*lx
         y = (iy-1)*dy + 0.5*dy - 0.5*ly
@@ -86,17 +92,19 @@ end
             end
         end
         Mus[ix,iy] = isinside ? musi : mus0
+        ρg[ix,iy]  = isinside ? ρgi  : ρg0
     end
     return
 end
 
 function generate_inclusions(lx,ly,nsub,ri)
-    li    = min(4*nsub*ri,lx-2.5*ri)
+    li    = min(3*nsub*ri,lx-2.5*ri)
     dx    = li/(nsub-1)
     dy    = dx*sqrt(3)/2
     if nsub == 1
         xs   = Float64[0]
         ys   = Float64[0]
+        # random offset
         jitx = [0.25*lx*(2*rand()-1)]
         jity = [0.25*ly*(2*rand()-1)]
     else
@@ -110,6 +118,7 @@ function generate_inclusions(lx,ly,nsub,ri)
                 push!(ys, oy + (j-1)*dy)
             end
         end
+        # random offset
         jitx = 0.5*(dx-2*ri) .* (2 .* rand(length(xs)) .- 1.0)
         jity = 0.5*(dy-2*ri) .* (2 .* rand(length(xs)) .- 1.0)
         #exponenial falloff
@@ -126,28 +135,27 @@ end
 
 @views function Stokes2D()
     # Set random seed for reproducibility
-    Random.seed!(1234)
+    Random.seed!(1855 + nsub)
+    # Set CUDA device
+    CUDA.device!(gpu_id)
     # Physics
-    lx, ly    = 10.0, 10.0  # domain extends
-    μs0       = 1.0         # matrix viscosity
-    μsi       = 10.0^(-vrpow) # inclusion viscosity
-    εbg       = 1.0         # background strain-rate
-    ri        = sqrt(lx*ly*0.025/π) # inclusion radius
+    lx, ly    = 10.0, 10.0          # domain extends
+    μs0       = 1.0                 # matrix viscosity
+    μsi       = 10.0^(-vrpow)       # inclusion viscosity
+    ri        = sqrt(lx*ly*0.005/π) # inclusion radius
     # Numerics
-    iterMax   = 1e5         # maximum number of pseudo-transient iterations
-    nout      = 500         # error checking frequency
-    ε         = 1e-7        # nonlinear absolute tolerence
-    CFL       = vrpow > 7 ? 0.6/sqrt(2) :
-                vrpow > 3 ? 0.7/sqrt(2) : 0.8/sqrt(2)
-    Re        = 5π*Re_mlt
-    r         = 1.0
+    iterMax   = 100max(nx,ny)       # maximum number of pseudo-transient iterations
+    nout      = 200                 # error checking frequency
+    ε_V       = 1e-5                # nonlinear absolute tolerence
+    ε_∇V      = 1e-3                # nonlinear absolute tolerence
+    CFL       = 0.9/sqrt(2)         # stability condition
+    Re        = 5π*Re_mlt           # Reynolds number                     (numerical parameter #1)
+    r         = 1.0                 # Bulk to shear elastic modulus ratio (numerical parameter #2)
     # Derived numerics
     dx, dy    = lx/nx, ly/ny # cell sizes
     max_lxy   = max(lx,ly)
     Vpdτ      = min(dx,dy)*CFL
-    nsm       = vrpow > 7 ? 25 :
-                vrpow > 5 ? 20 :
-                vrpow > 3 ? 15 : 10
+    nsm       = 10
     # Array allocations
     Pt        = @zeros(nx  ,ny  )
     ∇V        = @zeros(nx  ,ny  )
@@ -163,39 +171,46 @@ end
     Musτ      = @zeros(nx  ,ny  )
     Gdτ       = @zeros(nx  ,ny  )
     dτ_Rho    = @zeros(nx  ,ny  )
+    ρg        = @zeros(nx  ,ny  )
+    ρg2       = @zeros(nx  ,ny  )
     # Initial conditions
     Vx        =  zeros(nx+1,ny  )
     Vy        =  zeros(nx  ,ny+1)
     Vx        = Data.Array( -εbg.*[((ix-1)*dx -0.5*lx) for ix=1:size(Vx,1), iy=1:size(Vx,2)] )
     Vy        = Data.Array(  εbg.*[((iy-1)*dy -0.5*ly) for ix=1:size(Vy,1), iy=1:size(Vy,2)] )
     Xi,Yi     = Data.Array.(generate_inclusions(lx,ly,nsub,ri))
-    @parallel create_inclusions!(Mus,Xi,Yi,ri^2,μs0,μsi,dx,dy,lx,ly)
+    @parallel create_inclusions!(Mus,ρg,Xi,Yi,ri^2,μs0,μsi,ρg0,ρgi,dx,dy,lx,ly)
     Mus2     .= Mus
+    ρg2      .= ρg
     for _ = 1:nsm
         @parallel smooth!(Mus2, Mus, 1.0)
         Mus, Mus2 = Mus2, Mus
+        @parallel smooth!(ρg2, ρg, 1.0)
+        ρg, ρg2 = ρg2, ρg
     end
-    Musτ     .= Mus
-    @parallel compute_maxloc!(Musτ, Mus)
-    @parallel (1:size(Musτ,2)) bc_x!(Musτ)
-    @parallel (1:size(Musτ,1)) bc_y!(Musτ)
+    Musτ     .= μs0
     # Time loop
     @parallel compute_iter_params!(dτ_Rho, Gdτ, Musτ, Vpdτ, Re, r, max_lxy)
-    err=2*ε; iter=0; err_evo1=[]; err_evo2=[]
-    while err > ε && iter <= iterMax
+    err_V=2*ε_V; err_∇V=2*ε_∇V; iter=0; err_evo1=[]; err_evo2=[]
+    while !((err_V <= ε_V) && (err_∇V <= ε_∇V)) && (iter <= iterMax)
         if (iter==11)  global wtime0 = Base.time()  end
         @parallel compute_P!(∇V, Pt, Vx, Vy, Gdτ, r, dx, dy)
         @parallel compute_τ!(τxx, τyy, τxy, Vx, Vy, Mus, Gdτ, dx, dy)
-        @parallel compute_dV!(Rx, Ry, dVx, dVy, Pt, τxx, τyy, τxy, dτ_Rho, dx, dy)
+        @parallel compute_dV!(Rx, Ry, dVx, dVy, Pt, τxx, τyy, τxy, dτ_Rho, ρg, dx, dy)
         @parallel compute_V!(Vx, Vy, dVx, dVy)
         @parallel (1:size(Vx,1)) bc_y!(Vx)
         @parallel (1:size(Vy,2)) bc_x!(Vy)
         iter += 1
         if iter % nout == 0
-            norm_Rx = norm(Rx)/length(Rx); norm_Ry = norm(Ry)/length(Ry); norm_∇V = 0*norm(∇V)/length(∇V)
-            err = maximum([norm_Rx, norm_Ry, norm_∇V])
-            push!(err_evo1, maximum([norm_Rx, norm_Ry, norm_∇V])); push!(err_evo2,iter)
-            @printf("Total steps = %d, err = %1.3e [norm_Rx=%1.3e, norm_Ry=%1.3e, norm_∇V=%1.3e] \n", iter, err, norm_Rx, norm_Ry, norm_∇V)
+            Pmin, Pmax = minimum(Pt), maximum(Pt)
+            Vmin, Vmax = minimum(Vy), maximum(Vy)
+            norm_Rx    = norm(Rx)/(Pmax-Pmin)*lx/sqrt(length(Rx))
+            norm_Ry    = norm(Ry)/(Pmax-Pmin)*lx/sqrt(length(Ry))
+            norm_∇V    = norm(∇V)/(Vmax-Vmin)*lx/sqrt(length(∇V))
+            err_V      = maximum([norm_Rx, norm_Ry])
+            err_∇V     = norm_∇V
+            push!(err_evo1, maximum([norm_Rx, norm_Ry])); push!(err_evo2,iter)
+            @printf("Total steps = %d, err_V = %1.3e [norm_Rx=%1.3e, norm_Ry=%1.3e], err_∇V = %1.3e \n", iter, err_V, norm_Rx, norm_Ry, err_∇V)
         end
     end
     # Performance
@@ -203,7 +218,7 @@ end
     A_eff    = (3*2)/1e9*nx*ny*sizeof(Data.Number)  # Effective main memory access per iteration [GB] (Lower bound of required memory access: Te has to be read and written: 2 whole-array memaccess; Ci has to be read: : 1 whole-array memaccess)
     wtime_it = wtime/(iter-10)                      # Execution time per iteration [s]
     T_eff    = A_eff/wtime_it                       # Effective memory throughput [GB/s]
-    @printf("Total steps = %d, err = %1.3e, time = %1.3e sec (@ T_eff = %1.2f GB/s) \n", iter, err, wtime, round(T_eff, sigdigits=2))
+    @printf("Total steps = %d, time = %1.3e sec (@ T_eff = %1.2f GB/s) \n", iter, wtime, round(T_eff, sigdigits=2))
     # Visualisation
     if do_viz
         X, Y, Yv  = dx/2:dx:lx-dx/2, dy/2:dy:ly-dy/2, 0:dy:ly
@@ -213,14 +228,15 @@ end
         p5 = plot(err_evo2,err_evo1, legend=false, xlabel="# iterations", ylabel="log10(error)", linewidth=2, markershape=:circle, markersize=3, labels="max(error)", yaxis=:log10)
         display(plot(p1, p2, p4, p5))
     end
-    if do_save_vis && Re_mlt ≈ 1.0 && vrpow == 9
+    if do_save_vis && Re_mlt ≈ 1.0 && vrpow == 9 # workaround to avoid saving too much data
+        outdir = joinpath(simdir, "VISCR_$(vrpow)_NSUB_$(nsub)_REMULT_$(Re_mlt)")
         !ispath(outdir) && mkpath(outdir)
-        matwrite(joinpath(outdir,"Stokes2D.mat"), Dict("Pt_2D"=> Array(Pt), "Mus_2D"=> Array(Mus), "Txy_2D"=> Array(τxy), "Vy_2D"=> Array(Vy), "dx_2D"=> dx, "dy_2D"=> dy); compress = true)
+        matwrite(joinpath(outdir,"Stokes2D.mat"), Dict("Pt_2D"=> Array(Pt), "Mus_2D"=> Array(Mus), "Txy_2D"=> Array(τxy), "Txx_2D"=> Array(τxx), "Tyy_2D"=> Array(τyy), "Vx_2D"=> Array(Vx), "Vy_2D"=> Array(Vy), "dx_2D"=> dx, "dy_2D"=> dy); compress = true)
     end
     if do_save
-        !ispath(outdir) && mkpath(outdir)
-        open(joinpath(outdir,"out_Stokes2D.txt"),"a") do io
-            println(io, "$(nx) $(ny) $(iter) $(err)")
+        !ispath(simdir) && mkpath(simdir)
+        open(joinpath(simdir,"out_Stokes2D_param.txt"),"a") do io
+            println(io, "$(vrpow) $(nsub) $(Re_mlt) $(iter) $(err_V) $(err_∇V)")
         end
     end
     return
--- visu/viz_stokes_param.m
+++ visu/viz_stokes_param.m
@@ -1,44 +1,60 @@
-clear;figure(1);clf;colormap jet
-subs   = 1:5;
-nincls = [1 3 8 14 23];
-vrs    = 9:2:9;
-rems   = 0.5:0.1:1.5;
-lx     = 10;
-ly     = 10;
-tiledlayout(4,numel(subs),'TileSpacing','compact','Padding','compact')
-for isub = subs
-    for ivr = vrs
-        for irem = 1:numel(rems)
-            Remlt = rems(irem);
-            simdir = sprintf('../output/VISCR_%d/NSUB_%d/REMULT_%.1f',ivr,isub,Remlt);
-            if irem == numel(rems)
-                data_2D = load([simdir '/Stokes2D.mat']);
-                Pt_2D  = data_2D.Pt_2D;
-                Mu_2D  = data_2D.Mus_2D;
-                dx = data_2D.dx_2D; xc_2D = -lx/2+dx/2:dx:lx/2-dx/2;
-                dy = data_2D.dy_2D; yc_2D = -ly/2+dy/2:dy:ly/2-dy/2;
-                [x2D,y2D] = ndgrid(xc_2D,yc_2D);
-                nexttile;pcolor(x2D,y2D,Pt_2D);shading flat;axis image;colorbar
-                caxis([-5 5])
-                title(isub)
-            end
-            itdata = readmatrix([simdir '/out_Stokes2D_param.txt'],'FileType','text','Delimiter',' ');
-            ittot(isub,irem)  = itdata(3)/2048;
-            errtot(isub,irem) = itdata(4);
-        end
-    end
-    disp(isub)
+clear;figure(1);clf;load('roma.mat')
+%setup
+set(gcf,'Units','centimeters','Position',[12 2 12 18],'PaperUnits','centimeters','PaperPosition',[0 0 12 18]);
+fs = 9;
+% physics and numerics
+lx = 10; ly = 10; lz = 10; nx = 2048;
+% vis
+subs   = 1:3:7;
+vrs    = 1:4:9;
+rems   = 0.5:0.25:1.5;
+nincls = [1 14 46];
+data   = readmatrix('../output/out_Stokes2D_strong_param.txt');
+sz     = [numel(vrs),numel(subs),numel(rems)];
+Vr     = reshape(data(:,1),sz);
+Nsub   = reshape(data(:,2),sz);
+Re     = reshape(data(:,3),sz);
+Iters  = reshape(data(:,4),sz);
+tiledlayout(numel(subs),2,'TileSpacing','compact','Padding','normal')
+for iSub = 1:numel(subs)
+    % results
+    nexttile
+    load(sprintf('../output/VISCR_9/NSUB_%d/REMULT_1.0/Stokes2D_strong.mat',subs(iSub)))
+    dx = dx_2D; xc = dx/2:dx:lx-dx/2; xv = dx:dx:lx-dx;
+    dy = dy_2D; yc = dy/2:dy:ly-dy/2; yv = dy:dy:ly-dy;
+    [Xc,Yc] = meshgrid(xc,yc);
+    phi = nincls(iSub)*0.005;
+    rhog_eff = phi*0.5 + (1 - phi)*1;
+    dP = Pt_2D + rhog_eff*(Yc' - ly/2);
+    imagesc(xv,yv,dP');shading flat;axis image;caxis([-0.2 0.2])
+    hold on;
+    Vxc = 0.5*(Vx_2D(1:end-1,:)+Vx_2D(2:end,:));
+    Vyc = 0.5*(Vy_2D(:,1:end-1)+Vy_2D(:,2:end));
+    l=streamslice(Xc,Yc,Vxc',Vyc',1);
+    set(l,'Color','k','LineWidth',0.5);
+    hold off;
+    xlim([0 lx]);ylim([0 ly]);
+    yticks([0 ly]);ylabel('\bfly')
+    if iSub == 1; xticks([0 lx]);xlabel('\bflx');else; xticks([]); end
+    if iSub == numel(subs);cb1=colorbar('Location','southoutside'); cb1.Label.String = '\bfp';cb1.Label.FontSize=fs;end
+    text(-0.3,1.05,['\bf(' char('a'+iSub-1) ')'],'Units','normalized','FontName','Courier')
+    set(gca,'FontName','Courier','FontSize',fs,'YDir','normal','XAxisLocation','top')%,'ColorScale','log')
+    colormap(gca,roma)
+    % iter count
+    nexttile
+    imagesc(vrs,rems,squeeze(Iters(:,iSub,:))'/nx);shading flat
+    hold on
+    [~,I] = min(squeeze(Iters(:,iSub,:)),[],2);
+    Resc = squeeze(Re(:,iSub,:));
+    plot(vrs,rems(I),'w-o','LineWidth',1,'MarkerSize',2);
+    hold off
+    axis square;caxis([10 50]);
+    yticks(0.5:0.5:1.5);ylabel('\bfRe/Re_{opt}')
+    if iSub == 1;xticks(vrs);xlabel('\bf\mu^{0}_s/\mu^{inc}_s');else;xticks([]); end
+    if iSub == numel(subs);cb2=colorbar('Location','southoutside');cb2.Label.String='\bfiter_{tot}/nx';cb2.Label.FontSize=fs;end
+    set(gca,'FontName','Courier','FontSize',fs,'YDir','normal','XAxisLocation','top','YAxisLocation','right')
+    colormap(gca,flip(roma))
+    drawnow
 end
-[subs2,rems2] = ndgrid(nincls*2.5,rems);
-% nexttile([1 2]);
-contourf(subs2,rems2,ittot,10:20,'LineWidth',1);shading interp;caxis([10 20]);cb=colorbar;
-xlabel('Weak/Total area ratio, %')
-ylabel('Re/Re_{opt}')
-cb.Label.String = 'n_{iter}/n_x';cb.Label.FontSize = 14;
-cb.FontSize = 14;
-set(gca,'FontSize',14)
-[m,I] = min(ittot,[],2);
-hold on
-plot(nincls*2.5,rems(I),'w-','LineWidth',2)
-hold off
-drawnow
+cb2.Position(2)=cb1.Position(2);
+% exportgraphics(gcf,'fig_stokes_systematics_shear.png','Resolution',300)
